edceb0f727c19555baa1a8fe0cdeb66149aa52f5
Mithlesh04 <Mithlesh04>
2022-03-06 07:28:34 +0530

    pswd

--- server/db_conn.php
+++ server/db_conn.php
@@ -6,7 +6,7 @@ error_reporting(E_ERROR | E_PARSE);
 // Database connection
 $db_host = "127.0.0.1"; //change this to your database host name
 $db_user = "root"; //change this to your database user name
-$db_pass = "root123"; //change this to your database password
+$db_pass = "Root@123"; //change this to your database password
 $db_name = "aes";
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
